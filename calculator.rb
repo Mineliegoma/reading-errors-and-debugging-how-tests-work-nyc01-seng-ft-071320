04fb71e6420932fdd4b6e13e4d3edcda9c08a740
adept-bell-4492 <garlandegoma@gmail.com>
2020-06-28 03:45:20 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 # Add your variables here
-first_number.to_i = "Hello world!"
-second_number!=0 
+first_number = 6
+second_number=2
 sum=first_number + second_number
 difference= first_number-second_number
 product=first_number*second_number
